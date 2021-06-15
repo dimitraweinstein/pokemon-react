5b1b56379c8612143d405b823874d3516aefe7b8
Dimitra Weinstein <drenee.weinstein@gmail.com>
2021-06-14 22:50:25 -0700

    refactored state setting functions in to this.fetch

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -11,19 +11,18 @@ export default class Pokedex extends Component {
     }
 
     componentDidMount = async () => {
-        this.setState({ loading: true });
-        const response = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
-        
-        await sleep(1200)
-        this.setState({ loading: false });
-        this.setState({ pokedex: response.body.results });
+        await this.fetch();
     }
 
     handleClick = async () => {
+        await this.fetch();
+    }
+
+    fetch = async () => {
         this.setState({ loading: true });
         const response = await request.get('https://pokedex-alchemy.herokuapp.com/api/pokedex');
 
-        await sleep(1200)
+        await sleep(1500)
         this.setState({ loading: false });
         this.setState({ pokedex: response.body.results });
     }
@@ -35,14 +34,15 @@ export default class Pokedex extends Component {
                     <input onChange={this.handleChange} />
                     <button onClick={this.handleClick}>Fetch!</button>
 
-                    {this.state.loading && <Spinner />}
-
-                    { this.state.pokedex.map(pokemon => <div>
+                    {this.state.loading
+                        ? <Spinner />
+                        : this.state.pokedex.map(pokemon => 
+                    <div>
                         <p>{ pokemon.pokemon }</p>
                         <p>{ pokemon.type_2 }</p>
                         <p>{ pokemon.hp }</p>
                         <img src={ pokemon.url_image } alt="pokemon" />
-                    </div>) }
+                    </div>)}
                     
                 </section>
             </main>
